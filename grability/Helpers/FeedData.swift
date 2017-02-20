1edf27f9ddbcd7ea86520425960909d8fa2ffe79
¨Mario <mario.vizcainos@gmail.com>
2017-02-20 10:57:05 -0500

    sremoved unsused filed and added feedData

---
+++ grability/Helpers/FeedData.swift
@@ -0,0 +1,13 @@
+//
+//  FeedData.swift
+//  grability
+//
+//  Created by Mario Vizcaino on 20/02/17.
+//  Copyright © 2017 Mario Vizcaino. All rights reserved.
+//
+
+import Foundation
+
+class FeedData {
+    
+}
