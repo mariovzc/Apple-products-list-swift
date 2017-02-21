9dd996eb26e9f020ef13777be6531a01f1f4fa74
¨Mario <mario.vizcainos@gmail.com>
2017-02-21 18:34:36 -0500

    table methods

--- grability/iPhoneViews/iPhoneListVC.swift
+++ grability/iPhoneViews/iPhoneListVC.swift
@@ -10,9 +10,12 @@ import UIKit
 
 class iPhoneListVC: UIViewController {
 
+    let cellIdentifier = "cellIphone"
+    
     @IBOutlet weak var tableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        registerCellInTable()
 
         // Do any additional setup after loading the view.
     }
@@ -22,15 +25,64 @@ class iPhoneListVC: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func registerCellInTable() {
+        let nib:UINib = UINib(nibName: "iPhoneTableViewCell", bundle: nil)
+        self.tableView.register(nib, forCellReuseIdentifier: cellIdentifier)
     }
-    */
-
 }
+
+extension iPadDetailVC : UITableViewDataSource {
+    
+    private func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+        //return arrayData.count
+    }
+    
+    private func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: IndexPath) -> CGFloat {
+        let cell = iPhoneTableViewCell()
+        return cell.heigthCell
+    }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellIphone", for: indexPath) as! iPhoneTableViewCell
+        /*
+        cell.titleCell.text = arrayData[indexPath.row].name
+        cell.descriptionCell.text = arrayData[indexPath.row].price == "0.00000" ? "Gratis" : arrayData[indexPath.row].price
+        
+        let imgURL: NSURL = NSURL(string: arrayData[indexPath.row].imageUrl
+            )!
+        let request: NSURLRequest = NSURLRequest(url: imgURL as URL)
+        
+        NSURLConnection.sendAsynchronousRequest(request, queue: OperationQueue.mainQueue()) { (response, data, error) -> Void in
+            
+            if error == nil {
+                cell.imageCell.layer.cornerRadius = 5
+                cell.imageCell.image = UIImage(data: data!)
+            }
+        }*/
+        
+        return cell
+    }
+
+    
+    
+}
+
+
+extension iPadDetailVC : UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        //let obj = arrayData[indexPath.row]
+        
+    }
+    
+}
+
+
+
