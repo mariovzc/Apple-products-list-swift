77debc1907ec855ab93823221c5e218d58e40081
¨Mario <mario.vizcainos@gmail.com>
2017-02-21 18:16:41 -0500

    created iphone tableviewCell

---
+++ grability/iPhoneViews/iPhoneTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  iPhoneTableViewCell.swift
+//  grability
+//
+//  Created by Mario Vizcaino on 21/02/17.
+//  Copyright © 2017 Mario Vizcaino. All rights reserved.
+//
+
+import UIKit
+
+class iPhoneTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
