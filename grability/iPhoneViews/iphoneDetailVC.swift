b7797e3e3ae80664a436a7a4f45121acf589bb7f
¨Mario <mario.vizcainos@gmail.com>
2017-02-21 19:47:18 -0500

    added iPhone Detail view

---
+++ grability/iPhoneViews/iphoneDetailVC.swift
@@ -0,0 +1,56 @@
+//
+//  iphoneDetailVC.swift
+//  grability
+//
+//  Created by Mario Vizcaino on 21/02/17.
+//  Copyright © 2017 Mario Vizcaino. All rights reserved.
+//
+
+import UIKit
+
+class iphoneDetailVC: UIViewController {
+
+    @IBOutlet weak var appImage: UIImageView!
+    @IBOutlet weak var appName: UILabel!
+    @IBOutlet weak var appPrice: UILabel!
+    @IBOutlet weak var appDate: UILabel!
+    @IBOutlet weak var appSumary: UITextView!
+    var imageApp:UIImage?
+    var nameApp:String?
+    var priceApp:String?
+    var dateApp:String?
+    var sumaryApp:String?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        appImage.image = imageApp
+        appName.text = nameApp
+        appPrice.text = priceApp
+        appDate.text = dateApp
+        appSumary.text = sumaryApp
+
+        // Do any additional setup after loading the view.
+    }
+    init(withdata name: String, price: String, date: String, sumary: String, image: UIImage) {
+        super.init(nibName: nil, bundle: nil)
+        imageApp = image
+        priceApp = price == "0.00000" ? "Gratis" : price
+        nameApp = name
+        dateApp = date
+        sumaryApp = sumary
+        
+    }
+    
+    @IBAction func backButtonAction(_ sender: UIBarButtonItem) {
+        dismiss(animated: true, completion: nil)
+
+    }
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
